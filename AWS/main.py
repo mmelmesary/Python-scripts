3c9df47c6ca0749b92b4dbe94cb87317e420b396
melmesay <mohamed.elmasary123@gmail.com>
2023-07-07 03:51:48 +0300

    update print_vpcs.py file

--- AWS/main.py
+++ AWS/main.py
@@ -1,6 +1,6 @@
 import boto3
 
-ec2_client = boto3.client("ec2", region_name="us-west-1 ")
+ec2_client = boto3.client("ec2", region_name="us-east-2")
 
 all_avalable_vpcs = ec2_client.describe_vpcs()
 # print(all_avalable_vpcs["Vpcs"])
@@ -8,16 +8,7 @@ all_avalable_vpcs = ec2_client.describe_vpcs()
 vpcs = all_avalable_vpcs["Vpcs"]
 
 for vpc in vpcs:
-    print(vpc["CidrBlock"])
-    print(vpc["VpcId"])
-    print(vpc["CidrBlockAssociationSet"])
-
-    # print(vpc["CidrBlockAssociationSet"][0])
-    cidr_block_assoc_set = vpc["CidrBlockAssociationSet"] 
-    for assoc_set in cidr_block_assoc_set:
-        print (assoc_set["AssociationId"])
-        print (assoc_set["CidrBlockState"])
-        for state in assoc_set["CidrBlockState"]:
-            print (state)
-
+    print(f'vpc_id is => {vpc["VpcId"]}')
+    print(f"cidr_block is => {vpc['CidrBlock']}")
+    
 
